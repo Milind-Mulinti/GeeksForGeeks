afcb7b7d00cd37bec8c30eff3b9beed84bdb8c91
Milind Mulinti <milindmulinti@gmail.com>
2021-04-08 18:41:52 +0530

    first java class

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("This is first class created on github");
+        System.out.println("This is first class commiting and pushingon github through configured intelliJ");
     }
 }
